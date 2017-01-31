bca9557012af26d27da93e51fcd56498601bedb0
Vicente Ugarrio (B) <vugarrio@gmail.com>
2017-01-31 19:37:33 +0100

    lenovo - 4/16

---
+++ src/main/java/es/ugarrio/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package es.ugarrio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/say")
+public class HelloWorldController {
+	
+	@GetMapping("/helloworld")   //Nueva etiqueta de Spring 4
+	public String helloWorld () {
+		return "helloworld";
+	}
+	
+}
